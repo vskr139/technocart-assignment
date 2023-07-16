27cd6ae0fee3cb474c2487a0efcabce00479fc95
vskr139 <122347145+vskr139@users.noreply.github.com>
2023-07-16 18:35:15 +0530

    Update ConfigurationContext.js

--- src/context/ConfigurationContext.js
+++ src/context/ConfigurationContext.js
@@ -1,12 +1,1 @@
-import React from 'react'
 
-const ConfigurationContext = React.createContext({
-  showContent: true,
-  showLeftNavbar: true,
-  showRightNavbar: true,
-  onToggleShowContent: () => {},
-  onToggleShowLeftNavbar: () => {},
-  onToggleShowRightNavbar: () => {},
-})
-
-export default ConfigurationContext
